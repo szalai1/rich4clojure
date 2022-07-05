d676c91a76069547803c5f94c826d63109706ada
Peter Szalai <peter.szalai@askattest.com>
2022-07-05 08:33:59 +0000

    solve problem_063

--- src/rich4clojure/easy/problem_066.clj
+++ src/rich4clojure/easy/problem_066.clj
@@ -8,17 +8,20 @@
 ;; Given two integers, write a function which returns the
 ;; greatest common divisor.
 
-(def __ :tests-will-fail)
 
-(comment
-  
-  )
+(defn __ [a b]
+  (loop [a a b b]
+    (if (= (mod a b) 0)
+      b
+      (recur b (mod a b)))))
+
+(comment)
 
 (tests
-  (__ 2 4) := 2
-  (__ 10 5) := 5
-  (__ 5 7) := 1
-  (__ 1023 858) := 33)
+ (__ 2 4) := 2
+ (__ 10 5) := 5
+ (__ 5 7) := 1
+ (__ 1023 858) := 33)
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/4aa85a5a1d6e6962548e6dc69a02a55e
