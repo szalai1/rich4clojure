4176fd250a5e1304f471751c6282d8e2975eb0da
szalai1 <szalaipeti.vagyok@gmail.com>
2022-07-13 07:52:17 +0000

    solving 96

--- src/rich4clojure/easy/problem_096.clj
+++ src/rich4clojure/easy/problem_096.clj
@@ -13,25 +13,37 @@
 ;; not to Tree for a reminder on the tree representation
 ;; we're using).
 
-(def __ :tests-will-fail)
 
-(comment
-  
-  )
+
+(def __ (fn symetric? [[_ l r]]
+          (if (not (coll? l))
+            (= l r)
+            (if (not (coll? r)) false
+                (let [[lv ll lr] l
+                      [rv rl rr] r]
+                  (and (= lv rv)
+                       (symetric? [nil ll rr])
+                       (symetric? [nil lr rl])))))))
+
+
+
+
+
+(comment)
 
 (tests
-  (__ '(:a (:b nil nil) (:b nil nil))) := true
-  (__ '(:a (:b nil nil) nil)) := false
-  (__ '(:a (:b nil nil) (:c nil nil))) := false
-  (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
-          [2 [3 nil [4 [6 nil nil] [5 nil nil]]] nil]]) :=
-   true
-  (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
-          [2 [3 nil [4 [5 nil nil] [6 nil nil]]] nil]]) :=
-   false
-  (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
-          [2 [3 nil [4 [6 nil nil] nil]] nil]]) :=
-   false)
+ (__ '(:a (:b nil nil) (:b nil nil))) := true
+ (__ '(:a (:b nil nil) nil)) := false
+ (__ '(:a (:b nil nil) (:c nil nil))) := false
+ (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+      [2 [3 nil [4 [6 nil nil] [5 nil nil]]] nil]]) :=
+ true
+ (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+      [2 [3 nil [4 [5 nil nil] [6 nil nil]]] nil]]) :=
+ false
+ (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+      [2 [3 nil [4 [6 nil nil] nil]] nil]]) :=
+ false)
 
 ;; Share your solution, and/or check how others did it:
 ;; https://gist.github.com/74f30ddf0d4ce94f886ccf412d51a099
